135e0ec7f44418e7bd1d899821eda0fe49e008e5
lssguo <89848449+lssguo@users.noreply.github.com>
2021-12-16 13:12:41 +0800

    Minesweeper3.0
    扫雷系统基本完成，目前添加了鼠标移动事件

--- Minesweeper/script.js
+++ Minesweeper/script.js
@@ -6,13 +6,29 @@ function GameSetting(PaneRows,PaneCols,grid) {
             let tdSet = document.createElement("td") ;
             let SquareEl = document.createElement("div") ;
             SquareEl.className = "Square" ;
-            SquareEl.innerText = grid[x][y] ;
+            grid[x][y].SquareEl = SquareEl;
+
+            SquareEl.addEventListener("click", (e)=> {
+                if (grid[x][y].count === -1) {
+                   boom(grid, x, y, PaneRows, PaneCols)
+                   alert("boom!")
+                    return;
+                }
+
+                if (grid[x][y].count === 0) {
+                    OpenSpacse(grid, x, y, PaneRows, PaneCols)
+                }
+                else if (grid[x][y].count > 0) {
+                    grid[x][y].clear = true;
+                    SquareEl.classList.add("clear");
+                    grid[x][y].SquareEl.innerText = grid[x][y].count;
+                }
+
+                allcleared(grid);
+            });
+            
             tdSet.append(SquareEl) ;
             trSet.append(tdSet) ;
-
-
-
-            
         }
 
         boardSet.append(trSet) ;
@@ -25,22 +41,25 @@ const round = [
     [1, -1], [0, -1]
 ]
 
-function initialize(Rows, Cols, MinesNum) {
-    let pane = new Array(Rows) ;
-    for (let x = 0; x < Rows; x++) {
-        pane[x] = new Array(Cols);
-        for (let y = 0; y < Cols; y++){
-            pane[x][y] = 0
+function initialize(PaneRows, PaneCols, MinesNum) {
+    let pane = new Array(PaneRows) ;
+    for (let x = 0; x < PaneRows; x++) {
+        pane[x] = new Array(PaneCols);
+        for (let y = 0; y < PaneCols; y++){
+            pane[x][y] = {
+                clear: false,
+                count: 0
+            };
         }
     }
 
     let mines = [] ;
     for (let i = 0; i < MinesNum; i++) {
-        let row = Math.trunc(Math.random() * Rows) ;
-        let col = Math.trunc(Math.random() * Cols) ;
+        let row = Math.trunc(Math.random() * PaneRows) ;
+        let col = Math.trunc(Math.random() * PaneCols) ;
 
-        if (pane[row][col] != -1){
-            pane[row][col] = -1 ;
+        if (pane[row][col].count != -1){
+            pane[row][col].count = -1 ;
             mines.push([row, col]) ;
         }
         else {
@@ -53,22 +72,91 @@ function initialize(Rows, Cols, MinesNum) {
         for ( let [RRow, RCol] of round) {
             let NeighRow = row + RRow;
             let NeighCol = col + RCol;
-            if (NeighRow < 0 || NeighRow >= Rows || NeighCol < 0 || NeighCol >= Cols) {
+            if (NeighRow < 0 || NeighRow >= PaneRows || NeighCol < 0 || NeighCol >= PaneCols) {
                 continue;
             }
             
-            if (pane[NeighRow][NeighCol] === -1) {
+            if (pane[NeighRow][NeighCol].count === -1) {
                 continue;
             }
             else {
-                pane[NeighRow][NeighCol] += 1;
+                pane[NeighRow][NeighCol].count += 1;
             }
         }
     }
     return pane;
 }
 
+function boom(grid, x, y, PaneRows, PaneCols) {
+    grid[x][y].SquareEl.classList.add("boom");
 
+    for (let BoxRow = 0; BoxRow < PaneRows; BoxRow++) {
+        for (let BoxCols = 0; BoxCols < PaneCols; BoxCols++) {
+            let eachbox = grid[BoxRow][BoxCols];
+            eachbox.clear = true;
+
+            eachbox.SquareEl.classList.add("clear");
+
+            if (eachbox.count >0) {
+            eachbox.SquareEl.innerText = eachbox.count;
+            }
+            if (eachbox.count === -1) {
+                eachbox.SquareEl.classList.add("Unfound");
+            }
+        }
+    }
+}
+
+function OpenSpacse(grid, x, y, PaneRows, PaneCols) {
+    let zeroZone = grid[x][y];
+    zeroZone.clear = true;
+    zeroZone.SquareEl.classList.add("clear");
+
+    for (let [RRow, RCol] of round) {
+        let ZeroRow = x + RRow;
+        let ZeroCol = y + RCol;
+        
+        if (ZeroRow < 0 || ZeroRow >= PaneRows || ZeroCol < 0 || ZeroCol >= PaneCols) {
+            continue;
+        }
+
+        let ZeroRound = grid[ZeroRow][ZeroCol];
+        if (!ZeroRound.clear) {
+            ZeroRound.clear = true;
+            ZeroRound.SquareEl.classList.add("clear");
+            if (ZeroRound.count === 0) {
+                OpenSpacse(grid, ZeroRow, ZeroCol, PaneRows, PaneCols)
+            }
+            else if (ZeroRound.count > 0) {
+                ZeroRound.SquareEl.innerText = ZeroRound.count;
+            }
+        }
+    }
+}
+
+function allcleared(grid) {
+
+    for (let panerow = 0; panerow < grid.length; panerow ++) {
+        for (let panecol = 0; panecol < grid.length; panecol ++) {
+            let cell = grid[panerow][panecol];
+
+            if (cell.count !== -1 && !cell.clear){
+                return false;
+            }
+        }
+    }
+
+    for (let panerow = 0; panerow < grid.length; panerow ++) {
+        for (let panecol = 0; panecol < grid.length; panecol ++) {
+            let cell = grid[panerow][panecol];
+            if (cell.count === -1) {
+                cell.SquareEl.classList.add("Unfound");
+            }
+        }
+    }
+
+    return true;
+}
 
 let grid = initialize(9,9,9);
 
